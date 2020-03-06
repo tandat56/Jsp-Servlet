4e2d2b91c03017b69afc0556bc7c50107df8cd3c
tandat56 <datlt.letandat@gmail.com>
2020-03-06 19:44:38 +0700

    commit to repository - cấu hình theo mô hình 3 tier web architecture

--- Jsp-Servelt-Jdbc-2020/src/main/java/com/fpoly/controller/web/HomeController.java
+++ Jsp-Servelt-Jdbc-2020/src/main/java/com/fpoly/controller/web/HomeController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = {"/trang-chu"})
 public class HomeController extends HttpServlet {
 	
-	private static final long serialVersionUID = 2686801510274002166L;
+	private static final long serialVersionUID = 1L;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
---
+++ Jsp-Servelt-Jdbc-2020/src/main/java/com/fpoly/dao/impl/NewDAO.java
@@ -0,0 +1,8 @@
+package com.fpoly.dao.impl;
+
+import com.fpoly.dao.INewDAO;
+
+public class NewDAO implements INewDAO {
+
+	
+}
